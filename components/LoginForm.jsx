29a69bdd6410f565d5b24af059eaa7cc37693c76
IndraWinara <winaraindra@gmail.com>
2023-10-11 10:48:36 +0700

    Login-Auth-Frontend-done-hooks

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -1,26 +1,38 @@
+'use client'
+
 import Link from 'next/link'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
 const LoginForm = () => {
+const [email,setEmail] = useState('')
+const [password,setPassword] = useState('')
+const router = useRouter()
+
+const handleSubmit = (e)=>{
+e.preventDefault()
+router.push('/dashboard')
+}
+
     return (
         <div className='flex flex-col p-5 md:w-[500px] md:h-[400px] w-[300px] h-[400px] border-t-[4px] border-blue-500 border-[1px]  shadow-xl rounded-lg'>
             <div className='mx-auto'>
                 <h1 className='font-bold md:text-lg text-sm'>Welcome to our Awesome Project</h1>
             </div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='flex flex-col'>
                     <div className='flex flex-col p-2'>
                         <label className='text-sm md:text-[15px] font-bold'>E-mail :</label>
-                        <input type='email' placeholder='Email@email.com.....' className='p-2 text-sm md:text-[15px] outline-sky-500 border-[2px] rounded-lg border-sky-500' />
+                        <input value={email} onChange={(e)=>setEmail(e.target.value)} required type='email' placeholder='Email@email.com.....' className='p-2 text-sm md:text-[15px] outline-sky-500 border-[2px] rounded-lg border-sky-500' />
                     </div>
                     <div className='flex flex-col p-2'>
                         <label className='text-sm md:text-[15px] font-bold'>Password :</label>
-                        <input type='password' placeholder='*******' className='p-2 text-sm md:text-[15px] outline-sky-500 border-[2px] rounded-lg border-sky-500' />
+                        <input value={password} onChange={(e)=>setPassword(e.target.value)} required type='password' placeholder='*******' className='p-2 text-sm md:text-[15px] outline-sky-500 border-[2px] rounded-lg border-sky-500' />
                     </div>
                 </div>
                 <div className='flex flex-col justify-between h-[150px]'>
                     <div className='h-[50px] mt-5 flex justify-end'>
-                        <button className='bg-sky-300 font-bold hover:bg-white duration-200 md:w-full w-[100px] rounded-full border-[1px] border-sky-600'>Login</button>
+                        <button type='submit' className='bg-sky-300 font-bold hover:bg-white duration-200 md:w-full w-[100px] rounded-full border-[1px] border-sky-600'>Login</button>
                     </div>
                     <Link href='/register' className='text-sm text-end'>Don't have an account? <span className='underline cursor-pointer'>register</span></Link>
                 </div>
--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,30 +1,43 @@
+'use client'
+
 import Link from 'next/link'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
 const RegisterForm = () => {
+const [name,setName] = useState('')
+const [email,setEmail] = useState('')
+const [password,setPassword] = useState('')
+const router = useRouter()
+
+const handleSubmit = (e)=>{
+    e.preventDefault()
+    router.push('/')
+    }
+    
     return (
         <div className='flex flex-col p-5 md:w-[500px] md:h-[400px] w-[300px] h-[400px] border-t-[4px] border-blue-500 border-[1px]  shadow-xl rounded-lg'>
             <div className='mx-auto'>
                 <h1 className='font-bold md:text-lg text-sm'>Welcome to our Awesome Project</h1>
             </div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='flex flex-col'>
                     <div className='flex flex-col p-2'>
                         <label className='text-sm md:text-[15px] font-bold'>Name :</label>
-                        <input type='text' placeholder='John Doe...' className='p-2 text-sm md:text-[15px] outline-sky-500 border-[2px] rounded-lg border-sky-500' />
+                        <input required value={name} onChange={(e)=>setName(e.target.value)} type='text' placeholder='John Doe...' className='p-2 text-sm md:text-[15px] outline-sky-500 border-[2px] rounded-lg border-sky-500' />
                     </div>
                     <div className='flex flex-col p-2'>
                         <label className='text-sm md:text-[15px] font-bold'>E-mail :</label>
-                        <input type='email' placeholder='Email@email.com.....' className='p-2 text-sm md:text-[15px] outline-sky-500 border-[2px] rounded-lg border-sky-500' />
+                        <input required value={email} onChange={(e)=>setEmail(e.target.value)} type='email' placeholder='Email@email.com.....' className='p-2 text-sm md:text-[15px] outline-sky-500 border-[2px] rounded-lg border-sky-500' />
                     </div>
                     <div className='flex flex-col p-2'>
                         <label className='text-sm md:text-[15px] font-bold'>Password :</label>
-                        <input type='password' placeholder='*******' className='p-2 text-sm md:text-[15px] outline-sky-500 border-[2px] rounded-lg border-sky-500' />
+                        <input required value={password} onChange={(e)=>setPassword(e.target.value)} type='password' placeholder='*******' className='p-2 text-sm md:text-[15px] outline-sky-500 border-[2px] rounded-lg border-sky-500' />
                     </div>
                 </div>
                 <div className='flex flex-col justify-between h-[110px]'>
                     <div className='h-[50px] mt-5 flex justify-end'>
-                        <button className='bg-sky-300 font-bold hover:bg-white duration-200 md:w-full w-[100px] rounded-full border-[1px] border-sky-600'>Register</button>
+                        <button type='submit' className='bg-sky-300 font-bold hover:bg-white duration-200 md:w-full w-[100px] rounded-full border-[1px] border-sky-600'>Register</button>
                     </div>
                     <Link href='/' className='text-sm text-end'>Already have an account? <span className='underline cursor-pointer'>Login</span></Link>
                 </div>
